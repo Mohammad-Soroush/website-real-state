1bac28f41adba771e235e4f160f166689418cd84
Mohammad Soroush <mohammadsoroooush@gmail.com>
2025-04-22 11:06:32 +0330

    first

---
+++ Scripts/scripts.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let popupbt = document.getElementById("popup");
+    let closebt = document.querySelector(".close");
+    
+    setTimeout(function(){
+        popupbt.style.display = "block";
+        popupbt.style.justifyContent = "center";
+       
+    },5000);
+    closebt.addEventListener( "click" , function(){
+        popupbt.style.display = "none";
+    });
+});
+
+document.querySelector(".dropdownbtn").addEventListener('click', function(){
+    let dropdown = document.querySelector('.dropdown-content');
+    let arrow = document.querySelector('.arrow');
+    if(dropdown.style.display === "block"){
+        dropdown.style.display = "none";
+        arrow.classList.remove('rotate');
+    }else{
+        dropdown.style.display = "block";
+        dropdown.classList.add('rotate');
+    }
+});
+document.querySelector('.select-btn').addEventListener('click', function() {
+    let selected = [];
+    document.querySelectorAll('.dropdown-content input:checked').forEach(checkbox => {
+        selected.push(checkbox.value);
+    });
+ });
+
+document.querySelector('.delete-btn').addEventListener('click', function() {
+    document.querySelectorAll('.dropdown-content input:checked').forEach(checkbox => {
+        checkbox.checked = false;
+    });
+});
